f41063f800cdc2cef93f455f95e4083704c40f5c
Rob Amos <rob@robamos.co.uk>
2021-02-13 13:40:40 +0000

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-    purge: [
-        "./storage/framework/views/*.php",
-        "./resources/views/**/*.blade.php",
-    ],
+    // purge: [
+    //     "./storage/framework/views/*.php",
+    //     "./resources/views/**/*.blade.php",
+    //     options: {
+    //         whitelist: ['tip', 'warning', 'expert'],
+    //         whitelistPatternsChildren: [/content.$/]
+    //      }
+    // ],
     darkMode: "media", // or 'media' or 'class'
     theme: {
         colors,
