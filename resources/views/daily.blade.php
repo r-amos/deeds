33d225b5c976bd4f0693e2e1fe224da77ed41cb0
Rob Amos <rob@robamos.co.uk>
2021-02-13 15:41:19 +0000

    wip

--- resources/views/daily.blade.php
+++ resources/views/daily.blade.php
@@ -74,8 +74,28 @@
                     </div>
                 </div>
           </div>
-          <div class="invisible bg-red-500 bg-blue-500 bg-green-500"></div>
     </div>
-</div>
+    </div>
+    <div class="invisible
+    bg-purple-100 bg-purple-600 bg-purple-900 text-purple-500 text-purple-500
+        bg-red-100 bg-red-600 bg-red-900 text-red-500 text-red-500
+        bg-blue-100 bg-blue-600 bg-blue-900 text-blue-500 text-blue-500
+        bg-green-100 bg-green-600 bg-green-900 text-green-500 text-green-500
+        bg-yellow-100 bg-yellow-600 bg-yellow-900 text-yellow-500 text-yellow-500
+        bg-indigo-100 bg-indigo-600 bg-indigo-900 text-indigo-500 text-indigo-500
+        bg-pink-100 bg-pink-600 bg-pink-900 text-pink-500 text-pink-500 bg-gray-100
+        bg-gray-600 bg-gray-900 text-gray-500 text-gray-500
+        bg-orange-100 bg-orange-600 bg-orange-900 text-orange-500 text-orange-500
+        bg-amber-100 bg-amber-600 bg-amber-900 text-amber-500 text-amber-500
+        bg-lime-100 bg-lime-600 bg-lime-900 text-lime-500 text-lime-500
+        bg-emerald-100 bg-emerald-600 bg-emerald-900 text-emerald-500 text-emerald-500
+        bg-teal-100 bg-teal-600 bg-teal-900 text-teal-500 text-teal-500
+        bg-cyan-100 bg-cyan-600 bg-cyan-900 text-cyan-500 text-cyan-500
+        bg-lightBlue-100 bg-lightBlue-600 bg-lightBlue-900 text-lightBlue-500 text-lightBlue-500
+        bg-violet-100 bg-violet-600 bg-violet-900 text-violet-500 text-violet-500
+        bg-fuchsia-100 bg-fuchsia-600 bg-fuchsia-900 text-fuchsia-500 text-fuchsia-500
+        bg-rose-100 bg-rose-600 bg-rose-900 text-rose-500 text-rose-500
+    ">
+    </div>
   </body>
 </html>
